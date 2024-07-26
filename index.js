0e96eaf87c2a20c22c8b9a3b903f541483fea5ff
Oscar <rodroso@gmail.com>
2024-07-26 13:40:18 -0600

    Primer commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password: "",
+    database: "cotizador"
+});
+
+app.get("/getmargen",(req,res)=>{
+    const familia = req.query.familia;
+    const sucursal = req.query.sucursal;
+    db.query('SELECT margen FROM margenes WHERE familia=? AND sucursal=?',[familia,sucursal],
+        (err,result)=>{
+            if(err){
+                console.groupCollapsed(err);
+            }else{
+                res.send(result);
+                //console.log(result);
+            }
+        }
+    );
+})
+
+app.get("/getfamilias",(req,res)=>{
+    
+    db.query('SELECT DISTINCT familia FROM margenes order by familia',
+        (err,result)=>{
+            if(err){
+                console.groupCollapsed(err);
+            }else{
+                res.send(result);
+            }
+        }
+    );
+})
+
+app.listen(3001,()=>{
+    console.log("Corriendo en el puerto 3001")
+})
