0f0e83cdd4cbf3bffae671dc46e153d19836a1e7
Oscar <rodroso@gmail.com>
2024-08-16 00:29:06 -0600

    16

--- index.js
+++ index.js
@@ -7,101 +7,96 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-    host:"sealmarket.mx",
+    host: "sealmarket.mx",
     user: "sealmark_cotizauser",
     password: "Trof#4102",
     database: "sealmark_cotizador"
 });
 
 
-app.put("/actualiza",(req,res)=>{
-    /* console.log(req.method);
-    console.log(req.body);
-    console.log(req.body.fa);
-    console.log(req.body.du);
-    res.send();
- */
+app.put("/actualiza", (req, res) => {
+
     const familia = req.body.fa;
     const margenDgo = req.body.du;
     const margenFllo = req.body.fr;
     const margenMaz = req.body.ma;
     const margenZac = req.body.za;
     const margenTecmin = req.body.te;
-    const cambiaron=req.body.cambios;
-    const sucDgo="Durango";
-    const sucFllo="Fresnillo";
-    const sucMzt="Mazatlán";
-    const sucZac="Zacatecas";
-    const sucTec="Tecmin";
+    const cambiaron = req.body.cambios;
+    const sucDgo = "Durango";
+    const sucFllo = "Fresnillo";
+    const sucMzt = "Mazatlán";
+    const sucZac = "Zacatecas";
+    const sucTec = "Tecmin";
 
-    if(cambiaron.includes(sucDgo)){
-        console.log("Dentro de if");
-    }else{
-        console.log("No entró al if");
+    if (cambiaron.includes(sucDgo)) {
+        db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?', [margenDgo, sucDgo, familia],
+            (err, result) => {
+                if (err) {
+                    console.groupCollapsed(err);
+                } else {
+                    res.send(result);
+                }
+            }
+        );
     }
-
-    db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?' ,[margenDgo,sucDgo,familia],
-        (err,result)=>{
-            if(err){
-                console.groupCollapsed(err);
-            }else{
-                //res.send(result);
+    if (cambiaron.includes(sucFllo)) {
+        db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?', [margenFllo, sucFllo, familia],
+            (err, result) => {
+                if (err) {
+                    console.groupCollapsed(err);
+                } else {
+                    res.send(result);
+                }
             }
-        }
-    );
-    db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?',[margenFllo,sucFllo,familia],
-        (err,result)=>{
-            if(err){
-                console.groupCollapsed(err);
-            }else{
-                //res.send(result);
+        );
+    }
+    if (cambiaron.includes(sucMzt)) {
+        db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?', [margenMaz, sucMzt, familia],
+            (err, result) => {
+                if (err) {
+                    console.groupCollapsed(err);
+                } else {
+                    res.send(result);
+                }
             }
-        }
-    );
-    db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?',[margenMaz,sucMzt,familia],
-        (err,result)=>{
-            if(err){
-                console.groupCollapsed(err);
-            }else{
-                //res.send(result);
+        );
+    }
+    if (cambiaron.includes(sucZac)) {
+        db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?', [margenZac, sucZac, familia],
+            (err, result) => {
+                if (err) {
+                    console.groupCollapsed(err);
+                } else {
+                    res.send(result);
+                }
             }
-        }
-    );
-    db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?',[margenZac,sucZac,familia],
-        (err,result)=>{
-            if(err){
-                console.groupCollapsed(err);
-            }else{
-                //res.send(result);
+        );
+    }
+    if (cambiaron.includes(sucTec)) {
+        db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?', [margenTecmin, sucTec, familia],
+            (err, result) => {
+                if (err) {
+                    console.groupCollapsed(err);
+                } else {
+                    res.send(result);
+                }
             }
-        }
-    );
-    db.query('UPDATE margenes SET margen=? WHERE sucursal=? AND familia=?',[margenTecmin,sucTec,familia],
-        (err,result)=>{
-            if(err){
-                console.groupCollapsed(err);
-            }else{
-                res.send(result);
-            }
-        }
-    );
-
-
-
-
+        );
+    }
 
 })
 
 
 
-app.get("/getmargen",(req,res)=>{
+app.get("/getmargen", (req, res) => {
     const familia = req.query.familia;
     const sucursal = req.query.sucursal;
-    db.query('SELECT margen FROM margenes WHERE familia=? AND sucursal=?',[familia,sucursal],
-        (err,result)=>{
-            if(err){
+    db.query('SELECT margen FROM margenes WHERE familia=? AND sucursal=?', [familia, sucursal],
+        (err, result) => {
+            if (err) {
                 console.groupCollapsed(err);
-            }else{
+            } else {
                 res.send(result);
                 //console.log(result);
             }
@@ -109,32 +104,32 @@ app.get("/getmargen",(req,res)=>{
     );
 })
 
-app.get("/getfamilias",(req,res)=>{
-    
+app.get("/getfamilias", (req, res) => {
+
     db.query('SELECT DISTINCT familia FROM margenes order by familia',
-        (err,result)=>{
-            if(err){
+        (err, result) => {
+            if (err) {
                 console.groupCollapsed(err);
-            }else{
+            } else {
                 res.send(result);
             }
         }
     );
 })
 
-app.get("/margenes",(req,res)=>{
-    
+app.get("/margenes", (req, res) => {
+
     db.query('SELECT * FROM margenes',
-        (err,result)=>{
-            if(err){
+        (err, result) => {
+            if (err) {
                 console.groupCollapsed(err);
-            }else{
+            } else {
                 res.send(result);
             }
         }
     );
 })
 
-app.listen(3001,()=>{
+app.listen(3001, () => {
     console.log("Corriendo en el puerto 3001")
 })
