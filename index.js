64c325fbad0247a7086ea5eb787983ef706a6d89
Oscar <rodroso@gmail.com>
2024-10-02 11:48:36 -0600

    Precio5.0

--- index.js
+++ index.js
@@ -138,6 +138,19 @@ app.get("/margenes", (req, res) => {
     );
 })
 
+app.get("/getpreciosall", (req, res) => {
+
+    db.query('SELECT clave, precio, sucursal FROM precios',
+        (err, result) => {
+            if (err) {
+                console.groupCollapsed(err);
+            } else {
+                res.send(result);
+            }
+        }
+    );
+})
+
 app.get("/getprecios", (req, res) => {
     const sucursal = req.query.sucursal;
     db.query('SELECT clave, precio, precioIVA FROM preciosView WHERE sucursal=?', [sucursal],
