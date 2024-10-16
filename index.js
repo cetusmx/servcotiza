51f3fcb323a281e352023711cc14e5039fd93fb3
Oscar Rdz <118637313+cetusmx@users.noreply.github.com>
2024-10-16 15:45:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const cors = require("cors");
+var bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(express.json());
