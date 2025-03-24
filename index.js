e0224477f3b08170f4f5629b6bd1f978148279b4
Oscar Rdz <118637313+cetusmx@users.noreply.github.com>
2025-03-23 20:02:01 -0600

    6.0683

--- index.js
+++ index.js
@@ -265,14 +265,14 @@ app.post("/insertarLista", (req, res) => {
 
     const sucu = req.query.sucursal;
 
-    /* db.query('DELETE FROM precios WHERE sucursal=?', [sucu],
+    db.query('DELETE FROM precios WHERE sucursal=?', [sucu],
         (err, result) => {
             if (err) {
                 return res.send(err)
                 console.log(err)
             }
         }
-    ); */
+    );
 
     //console.log(req.body);
 
