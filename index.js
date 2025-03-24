6fa9b51bdc3f6680cf68bcf08f11a3794050a560
Oscar Rdz <118637313+cetusmx@users.noreply.github.com>
2025-03-23 21:15:30 -0600

    6.0687

--- index.js
+++ index.js
@@ -203,32 +203,6 @@ app.get("/borrarMargenes", (req, res) => {
     );
 })
 
-/* app.post("/insertarLista", (req, res) => {
-    console.log(req.body);
-
-    req.body.map(element => {
-        console.log(element);
-
-        const clave = element.Clave;
-        const precio = element.Precio;
-        const sucursal = element.Sucursal;
-
-        console.log(clave + "-" + precio + "-" + sucursal);
-
-        db.query('INSERT INTO precios(clave,precio,sucursal) values(?,?,?)', [clave, precio, sucursal],
-            (err, result) => {
-                if (err) {
-                    return res.send(err)
-                    console.log(err)
-                }
-            }
-        );
-
-    });
-
-    return res.send("INSERTED");
-}) */
-
 app.post("/insertarSiembra", (req, res) => {
     console.log(req.body);
 
