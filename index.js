ee51af961823e6e685f25dd7b2b62d90b7ed1d48
Oscar Rdz <118637313+cetusmx@users.noreply.github.com>
2025-06-01 00:47:23 -0600

    7.28

--- index.js
+++ index.js
@@ -356,7 +356,7 @@ app.get("/getresumeninventarios", (req, res) => {
 
 app.get("/getresumeninventario", (req, res) => {
     const InventarioID = req.query.InventarioID;
-    console.log(id);
+    console.log(InventarioID);
     db.query('SELECT InventarioID, qtyProductos, Ciudad, Almacen, Fecha, qtyLineas, ProgressPorcentage FROM inv_resumen_inventarios_app_view WHERE InventarioID=?',[InventarioID],
         (err, result) => {
             if (err) {
